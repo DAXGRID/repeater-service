28232f9bfd98649ef6e5ad05413dc77b45f81dca
Rune Nielsen <runenielsen@runbox.com>
2022-04-04 09:23:41 +0200

    setup background worker

---
+++ src/RepeaterService/Config/HostConfig.cs
@@ -0,0 +1,60 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace RepeaterService.Config;
+
+public static class HostConfig
+{
+    public static IHost Configure()
+    {
+        var hostBuilder = new HostBuilder();
+        ConfigureApp(hostBuilder);
+        ConfigureLogging(hostBuilder);
+        ConfigureServices(hostBuilder);
+        return hostBuilder.Build();
+    }
+
+    private static void ConfigureApp(IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            config.SetBasePath(System.AppContext.BaseDirectory);
+            config.AddJsonFile("appsettings.json", false, true);
+        });
+    }
+
+    private static void ConfigureServices(IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices((hostContext, services) =>
+        {
+            services.AddOptions();
+            services.AddHostedService<RepeaterServiceHost>();
+        });
+    }
+
+    private static void ConfigureLogging(IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices((hostContext, services) =>
+        {
+            var loggingConfiguration = new ConfigurationBuilder()
+               .AddEnvironmentVariables().Build();
+
+            services.AddLogging(loggingBuilder =>
+            {
+                var logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(loggingConfiguration)
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .MinimumLevel.Override("System", LogEventLevel.Warning)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(new CompactJsonFormatter())
+                    .CreateLogger();
+
+                loggingBuilder.AddSerilog(logger, true);
+            });
+        });
+    }
+}
